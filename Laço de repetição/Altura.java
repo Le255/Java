e70907967f3ee32465cb5a9de56412c597dba84b
Le255 <leticia.silva2374@etec.sp.gov.br>
2024-09-12 20:22:46 -0300

    Add files via upload

---
+++ Laço de repetição/Altura.java
@@ -0,0 +1,22 @@
+package lacoderepeticao;
+
+public class Altura {
+	public static void main(String[] args) {
+		double j,p,dj,dp,i=1;
+		j=1.34;
+		p=1.45;
+		dj=0.025;
+		dp=0.02;
+ 
+		while (p>j) {
+	           System.out.println("joão ficará mais alto que pedro em " +i+ " anos");
+	           p=p+dp;
+	           j=j+dj;
+	           i++;
+ 
+		}
+ 
+ 
+		}
+	}
+
