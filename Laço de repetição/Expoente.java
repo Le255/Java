386f88f32f658fa3cffaf2a52fec4dee12059c0a
Le255 <leticia.silva2374@etec.sp.gov.br>
2024-09-12 20:23:04 -0300

    Add files via upload

---
+++ Laço de repetição/Expoente.java
@@ -0,0 +1,22 @@
+package lacoderepeticao;
+import java.util.*;
+public class Expoente {
+	public static void main(String[] args) {
+		Scanner ler= new Scanner (System.in);
+		int b, ex, p=1, i=0;
+		
+		System.out.println("Digite a base");
+		b=ler.nextInt();
+		
+		System.out.println("Digite o expoente");
+		ex=ler.nextInt();
+		
+		while (i<ex) {
+			p=p*b;
+			i++;
+			
+			ler.close();
+		}
+		System.out.println("O resultado da potência é:" +p);
+	}
+}
