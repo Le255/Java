3137b99439f3b78f5e3e85344072b03898160a48
Le255 <leticia.silva2374@etec.sp.gov.br>
2024-09-12 20:39:38 -0300

    Add files via upload

---
+++ Começo do ano/veiculo.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class veiculo {
+public static void main(String[] args) {
+	Scanner ler=new Scanner(System.in);
+	double preco ,ipva;
+	System.out.println("digite o preço");
+	preco= ler.nextDouble();
+	ipva= 100*0.04;
+	System.out.println("o valor do ipva é :" +ipva);
+	
+	
+	
+}
+}
