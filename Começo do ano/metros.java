f6a2f3f6f94279174b05ef6f53457852823bff07
Le255 <leticia.silva2374@etec.sp.gov.br>
2024-09-12 20:40:12 -0300

    Add files via upload

---
+++ Começo do ano/metros.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class metros {
+	public static void main(String[] args) {
+		Scanner ler =new Scanner (System.in);
+		int metro,quilometro;
+		System.out.println("digite quantos metros: ");
+		metro=ler.nextInt();
+		quilometro =metro/1000;
+		System.out.println("metros convertidos :" +quilometro);
+			
+		
+		
+	}
+
+}
