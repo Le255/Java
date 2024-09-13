0494b022bc3f6766fa331410878a327dae9455a2
Le255 <leticia.silva2374@etec.sp.gov.br>
2024-09-13 09:00:26 -0300

    Add files via upload

---
+++ Classicos (Laço de repetição)/Aritmetica.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Aritmetica {
+public static void main(String[] args) {
+		
+		Scanner ler= new Scanner (System.in);
+		double itInicial, itFinal, soma=0, m=0;
+		int i=0;
+		
+		
+		System.out.println("Digite o intervalo inicial");
+		itInicial=ler.nextDouble();
+		
+		System.out.println("Digite o intervalo final");
+		itFinal=ler.nextDouble();
+		
+		while (itInicial<=itFinal) {
+		i++;
+		soma= soma + itInicial;
+		itInicial++;
+		}
+		m= soma/i;
+		System.out.println("sua media é:" +m);
+		soma=ler.nextInt();
+	}
+ 
+	
+}
