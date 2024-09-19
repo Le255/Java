3afbad1a0fb97628e3bb56758d5f1ff1f582552d
Le255 <leticia.silva2374@etec.sp.gov.br>
2024-09-19 17:19:16 -0300

    Add files via upload

---
+++ For (Laço de repetição)/Multiplode10.java
@@ -0,0 +1,15 @@
+
+public class Multiplode10 {
+	public static void main(String[] args) {
+		int i=0;
+		
+		for (i=0; i<=100; i++) {
+			
+			if (i%10 == 0) {
+				System.out.println(i+ " Esse número é um múltiplo de 10" );
+			}else {
+				System.out.println(i);
+			}
+		}
+	}
+}
